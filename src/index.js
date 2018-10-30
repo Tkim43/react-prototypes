220c5362169aa97ad991aace1b8973143c97297c
Tiffany Kim <minikim9@hotmail.com>
2018-10-29 18:10:01 -0700

    completed 1st proto

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const userObj = {name: 'tiffany', luckyNumber: luckyNumber()};
+// make sure you have two parameters
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(userObj),
     document.getElementById('root')
 );
+
+function luckyNumber(){
+    // 0 - 1000
+    // return Math.floor(Math.random() * (1000+1));
+    // 1 - 1000
+    return Math.floor(Math.random() * 1000) +1;
+}
+
+
+function greeting(obj){
+    return (
+    <div className='container'>
+        <h1>Welcome {obj.name}</h1>
+        <h2 className ='text-muted'>Your lucky number is: {obj.luckyNumber}</h2>
+    </div>)
+}
