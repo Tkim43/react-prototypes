236e22fc077ecdbbab93ea71a390787929416748
Tiffany Kim <minikim9@hotmail.com>
2018-11-08 15:14:58 -0800

    workin on feature set 2

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,14 @@
+import React from 'react'
+import { Route } from 'react-router-dom';
+import Welcome from './welcome';
 
+export default () =>{
+    return(
+        <div className="container">
+            <h1>some text</h1>
+            <Welcome/>
+            <Route path ="/" component ={Welcome}/>
+        </div>
+)
+}
 
---
+++ src/components/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props =>{
+    return(
+        <div>
+            <h1>Welcome to This Yo</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet impedit nam non obcaecati quam quo vitae. Assumenda atque dicta dolores enim, fugit, ipsam molestias obcaecati quisquam recusandae tempora tempore voluptatibus?</p>
+        </div>
+    )
+}
