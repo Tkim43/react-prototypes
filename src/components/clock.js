20d382792ff25285e9e7ca81201cd7bb0250305e
Tiffany Kim <minikim9@hotmail.com>
2018-11-19 11:37:01 -0800

    redux-intro completed

--- src/components/clock.js
+++ src/components/clock.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import {Component} from 'react'
 import {connect} from 'react-redux'
+import {updateTime} from '../actions/index'
 
 class Clock extends Component{
+    componentDidMount(){
+        // setInterval(this.props.updateTime,1000);
+        setInterval(()=>{
+            this.props.updateTime();
+        },1000);
+    }
     render(){
         return(
             <div className ="text-center mt-5">
-                <h1>Clock out time</h1>
+                <h1>{this.props.time}</h1>
             </div>
         )
     }
@@ -17,4 +24,4 @@ function mapStateProps(state){
         time: state.clock.time
     }
 }
-export default connect(mapStateProps(Clock);
+export default connect(mapStateProps,{updateTime: updateTime})(Clock);
